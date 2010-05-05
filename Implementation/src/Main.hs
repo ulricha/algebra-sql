b1dae2e2344af6d1c0e758a966c97c9775f39a0b
Jeroen Weijers <jweijers@union.informatik.uni-tuebingen.de>
2010-05-05 16:33:15 +0200

    Comments in Main

--- Implementation/src/Main.hs
+++ Implementation/src/Main.hs
@@ -1,26 +1,42 @@
+-- | This class forms the main executable unit of the Ferry 2.0 compiler
 module Main where
 
 import System.Console.GetOpt
 import System.Environment
 import Ferry.Front.Parser.Parser
 
-
+-- | The config datatype is used to store program flags given by the user 
+--   The compiler can be put in a 'Mode' that determines what sort of
+--   result the compilation process will result in.
+--   The 'Input' element is set to specify whether a file should be compiled or 
+--   input from the stdin
+--   The debug component is set to switch on debugging mode, debugging mode
+--   results in log information on the stdin and possibly extra compiler artifacts.
 data Config = Config {
               mode :: Mode,
               input :: Input,
               debug :: Bool
             }
 
-data Mode = Echo | Parse | Pretty
-data Input = File | Arg
+-- | The modes that are supported by the compiler.
+--   run ferryc -h to see a list of all options
+data Mode = Echo   -- ^ Echo mode prints the given input to the console
+          | Parse  -- ^ Parse mode will stop the compiler after the parsing phase
+          | Pretty -- ^ Pretty mode parses the given input and pretty prints the result
 
+-- | The input mode determines whether the source program is given through a file or via stdin
+data Input  = File -- ^ File mode, the program is read from a file 
+            | Arg  -- ^ Argument mode, the program is given as input directly
+
+-- | The default configuration for the compiler
 defaultConfig :: Config
 defaultConfig = Config {
-                mode        = Parse,
-                input       = File,
-                debug       = False
+                mode        = Parse, -- ^ Standard 'Mode' is set to Parse
+                input       = File,  -- ^ By default the program is given through a File
+                debug       = False  -- ^ Debug turned of by default
               }
 
+-- | Description of the options for the compiler 'Mode'
 options :: [OptDescr (Config -> Config)]
 options = [ Option ['i'] ["input"]
                    (NoArg (\o -> o {input = Arg}))
@@ -39,6 +55,7 @@ options = [ Option ['i'] ["input"]
                     "Print debugging information from heuristics."
           ]
 
+-- | Process the arguments given to the compiler
 processArgs :: String -> [String] -> IO (Config, [String])
 processArgs progName args =
   case getOpt Permute options args of
@@ -48,6 +65,7 @@ processArgs progName args =
     header = "\nUsage: " ++ progName
              ++ " [OPTION...] [FILES], with the following options:"
 
+-- | Main thread
 main :: IO ()
 main = 
     do
@@ -56,6 +74,8 @@ main =
         (opts, inp) <- processArgs progName args
         compile opts inp
 
+-- | The compiler pipeline
+--   Note that there should be a monadic style for handling all the steps in the pipeline
 compile :: Config -> [String] -> IO ()
 compile opts inp = do
                         src <- case (input opts) of
