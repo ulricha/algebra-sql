c81c67b625fac4b548a3e6a6e7bfc0fee3c8f4c7
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-07-08 09:58:34 +0200

    Moved some `more generic` stuff to a seperate module

---
+++ Implementation/src/Ferry/TypedCore/Rewrite/Combinators.hs
@@ -0,0 +1,68 @@
+module Ferry.TypedCore.Rewrite.Combinators where
+
+import Ferry.TypedCore.Data.Type
+import Ferry.TypedCore.Data.TypedCore
+import Ferry.TypedCore.Data.Instances
+import qualified Data.List as L
+    
+-- | Count variable node, needs a specialized type in the AST and therefore still expects the type of the list
+countF :: Qual FType -> CoreExpr
+countF (q :=> t) = Var (q :=> t .-> int) "count"  
+
+-- | Wrap an expression that is to be passed as an argument to a function
+wrapArg :: CoreExpr -> Param
+wrapArg e = ParExpr (typeOf e) e
+
+eq :: CoreExpr -> CoreExpr -> CoreExpr
+eq e1 e2 = let (q1 :=> t1) = typeOf e1
+               (q2 :=> t2) = typeOf e2
+            in BinOp (q1 `L.union` q2 :=> t1 .-> t2 .-> FBool) (Op "==") e1 e2
+
+notF :: CoreExpr -> CoreExpr
+notF e = App ([] :=> FBool) (Var ([] :=> FBool .-> FBool) "not") (ParExpr (typeOf e) e)    
+
+lengthF :: CoreExpr -> CoreExpr
+lengthF e = let (q :=> t) = typeOf e
+            in App ([] :=> FInt) (Var (q :=> t .-> FInt) "length") (ParExpr (typeOf e) e)
+
+minPF :: CoreExpr -> CoreExpr -> CoreExpr
+minPF e1 e2 = let (q1 :=> t1) = typeOf e1
+                  (q2 :=> t2) = typeOf e2
+                  fn = Var (q1 `L.union` q2 :=> t1 .-> t2 .-> FInt) "minP"
+                  app1 = App (q2 :=> t2 .-> FInt) fn (ParExpr (typeOf e1) e1)
+               in App ([] :=> FInt) app1 (ParExpr (typeOf e2) e2)
+               
+-- | Create the zip variable node with specialized function type
+zipF :: Qual FType -> Qual FType -> CoreExpr
+zipF (q1 :=> FList t1) (q2 :=> FList t2) = Var ((q1 `L.union` q2) :=> FList t1 .-> FList t2 .-> (FList $ rec [(RLabel "1", t1), (RLabel "2", t2)])) "zip"
+
+-- | Create a typed let binding node                                          
+binding :: String -> CoreExpr -> CoreExpr -> CoreExpr
+binding s e eb = Let (typeOf eb) s e eb
+
+-- | Zip two list
+zipC :: CoreExpr -> CoreExpr -> CoreExpr
+zipC e1 e2 = let ty1@(q1 :=> t1) = typeOf e1
+                 ty2@(q2 :=> t2) = typeOf e2
+                 zipV = zipF ty1 ty2
+                 (q :=> zipT) = zippedTy ty1 ty2 
+                 app1T = q :=> t2 .-> zipT
+              in App (q :=> zipT) (App app1T zipV (ParExpr ty1 e1)) (ParExpr ty2 e2)
+              
+-- | All variable node                   
+allN :: CoreExpr
+allN = Var ([] :=> list FBool .-> FBool) "all"
+
+mapN :: Qual FType -> Qual FType -> CoreExpr
+mapN (q1 :=> t1) (q2 :=> t2) = Var (q1 `L.union` q2 :=> t1 .-> t2 .-> list FBool) "map"
+
+-- | Chain two boolean expression together in an and relation    
+andExpr :: CoreExpr -> CoreExpr -> CoreExpr
+andExpr = BinOp ([] :=> FBool) (Op "&&")
+
+orExpr :: CoreExpr -> CoreExpr -> CoreExpr
+orExpr = BinOp ([] :=> FBool) (Op "||")
+
+-- | Return the type that two lists that are zipped would result in
+zippedTy :: Qual FType -> Qual FType -> Qual FType
+zippedTy (q1 :=> (FList t1)) (q2 :=> (FList t2)) = (q1 `L.union` q2) :=> (FList $ rec [(RLabel "1", t1), (RLabel "2", t2)])
--- Implementation/src/Ferry/TypedCore/Rewrite/OpRewrite.hs
+++ Implementation/src/Ferry/TypedCore/Rewrite/OpRewrite.hs
@@ -4,6 +4,7 @@ import Ferry.TypedCore.Data.Type
 import Ferry.TypedCore.Data.TypedCore
 import Ferry.TypedCore.Convert.Traverse
 import Ferry.TypedCore.Data.Instances
+import Ferry.TypedCore.Rewrite.Combinators
 
 import qualified Data.List as L
 
@@ -90,9 +91,6 @@ ordList id1 id2 val1 val2 = let t1@(q1 :=> ty1@(FList ls1)) = typeOf val1
                                 ltMinPf = BinOp ([] :=> FBool) (Op "<") (minPF var1 var2) $ minPF var2 var1
                              in rewrite' $ flip orExpr ltMinPf $ andExpr lens eqMinPf
                          
-notEq :: CoreExpr -> CoreExpr -> Rewrite CoreExpr
-notEq e1 e2 = rewrite' $ notF (BinOp ([] :=> FBool) (Op "==") e1 e2)
-              
 eqRecExpr :: String -> String -> CoreExpr -> CoreExpr -> Rewrite CoreExpr
 eqRecExpr id1 id2 val1 val2 = do
                                 let t1@(q1 :=> ty1@(FRec ls1)) = typeOf val1
@@ -120,13 +118,6 @@ eqListExpr id1 id2 val1 val2 = do
                                 elEq <- elemEq var1 var2
                                 return $ andExpr (eqLength var1 var2) elEq
     
--- | Chain two boolean expression together in an and relation    
-andExpr :: CoreExpr -> CoreExpr -> CoreExpr
-andExpr = BinOp ([] :=> FBool) (Op "&&")
-
-orExpr :: CoreExpr -> CoreExpr -> CoreExpr
-orExpr = BinOp ([] :=> FBool) (Op "||")
-
 -- | Given two list expressions returns an expression that checks that they have equal length
 eqLength :: CoreExpr -> CoreExpr -> CoreExpr
 eqLength e1 e2 = let (q1 :=> t1) = typeOf e1
@@ -148,13 +139,6 @@ elemEq e1 e2 = do
                 let app2 = App ([] :=> list FBool) app1 (ParExpr (qE :=> tE) zipE)
                 return $ App ([] :=> FBool) allN (ParExpr (typeOf app2) app2) 
 
--- | All variable node                   
-allN :: CoreExpr
-allN = Var ([] :=> list FBool .-> FBool) "all"
-
-mapN :: Qual FType -> Qual FType -> CoreExpr
-mapN (q1 :=> t1) (q2 :=> t2) = Var (q1 `L.union` q2 :=> t1 .-> t2 .-> list FBool) "map"
-                   
 eqAbstr :: Qual FType -> Qual FType -> Rewrite Param
 eqAbstr ty1@(q1 :=> FList t1) ty2@(q2 :=> FList t2) = 
           do
@@ -166,52 +150,5 @@ eqAbstr ty1@(q1 :=> FList t1) ty2@(q2 :=> FList t2) =
             eqE <- rewrite' $ BinOp ([] :=> FBool) (Op "==") el1 el2
             return $ ParAbstr (q :=> t .-> FBool) (PVar f) eqE    
 
--- | Create a typed let binding node                                          
-binding :: String -> CoreExpr -> CoreExpr -> CoreExpr
-binding s e eb = Let (typeOf eb) s e eb
-
--- | Zip two list
-zipC :: CoreExpr -> CoreExpr -> CoreExpr
-zipC e1 e2 = let ty1@(q1 :=> t1) = typeOf e1
-                 ty2@(q2 :=> t2) = typeOf e2
-                 zipV = zipF ty1 ty2
-                 (q :=> zipT) = zippedTy ty1 ty2 
-                 app1T = q :=> t2 .-> zipT
-              in App (q :=> zipT) (App app1T zipV (ParExpr ty1 e1)) (ParExpr ty2 e2)
-
--- | Return the type that two lists that are zipped would result in
-zippedTy :: Qual FType -> Qual FType -> Qual FType
-zippedTy (q1 :=> (FList t1)) (q2 :=> (FList t2)) = (q1 `L.union` q2) :=> (FList $ rec [(RLabel "1", t1), (RLabel "2", t2)])
-
-
--- | Create the zip variable node with specialized function type
-zipF :: Qual FType -> Qual FType -> CoreExpr
-zipF (q1 :=> FList t1) (q2 :=> FList t2) = Var ((q1 `L.union` q2) :=> FList t1 .-> FList t2 .-> (FList $ rec [(RLabel "1", t1), (RLabel "2", t2)])) "zip"
-
-
--- | Count variable node, needs a specialized type in the AST and therefore still expects the type of the list
-countF :: Qual FType -> CoreExpr
-countF (q :=> t) = Var (q :=> t .-> int) "count"  
-
--- | Wrap an expression that is to be passed as an argument to a function
-wrapArg :: CoreExpr -> Param
-wrapArg e = ParExpr (typeOf e) e
-
-eq :: CoreExpr -> CoreExpr -> CoreExpr
-eq e1 e2 = let (q1 :=> t1) = typeOf e1
-               (q2 :=> t2) = typeOf e2
-            in BinOp (q1 `L.union` q2 :=> t1 .-> t2 .-> FBool) (Op "==") e1 e2
-
-notF :: CoreExpr -> CoreExpr
-notF e = App ([] :=> FBool) (Var ([] :=> FBool .-> FBool) "not") (ParExpr (typeOf e) e)    
-
-lengthF :: CoreExpr -> CoreExpr
-lengthF e = let (q :=> t) = typeOf e
-            in App ([] :=> FInt) (Var (q :=> t .-> FInt) "length") (ParExpr (typeOf e) e)
-            
-minPF :: CoreExpr -> CoreExpr -> CoreExpr
-minPF e1 e2 = let (q1 :=> t1) = typeOf e1
-                  (q2 :=> t2) = typeOf e2
-                  fn = Var (q1 `L.union` q2 :=> t1 .-> t2 .-> FInt) "minP"
-                  app1 = App (q2 :=> t2 .-> FInt) fn (ParExpr (typeOf e1) e1)
-               in App ([] :=> FInt) app1 (ParExpr (typeOf e2) e2)
+notEq :: CoreExpr -> CoreExpr -> Rewrite CoreExpr
+notEq e1 e2 = rewrite' $ notF (BinOp ([] :=> FBool) (Op "==") e1 e2)
