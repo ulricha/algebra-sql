5ffaa0395495f67a17a86f6f173c792e8430f75f
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-07-19 08:10:44 +0200

    removed all traces of fgl

--- Implementation/src/Ferry/Algebra/Data/GraphBuilder.hs
+++ Implementation/src/Ferry/Algebra/Data/GraphBuilder.hs
@@ -1,10 +1,6 @@
 {-# LANGUAGE GADTs #-}
 module Ferry.Algebra.Data.GraphBuilder where
     
-import Data.Graph.Inductive.Graph
-import Data.Graph.Inductive.Basic
-import Data.Graph.Inductive.Tree
-
 import Ferry.Algebra.Data.Algebra
 import Ferry.Algebra.Data.Create
 
@@ -12,13 +8,7 @@ import qualified Data.Map as M
 import Control.Monad.State
 import Control.Monad.Reader
 
-type AlgGr = Gr Algebra ()
-
-type Link = Adj ()
-
-type ContextN = Context Algebra ()
-
-type GraphM = ReaderT (Gam, AlgNode) (State (Int, AlgGr, M.Map AlgNode Int))
+type GraphM = ReaderT (Gam, AlgNode) (State (Int, M.Map AlgNode Int))
 
 type Gam = [(String, Int)]
 
@@ -35,7 +25,7 @@ initLoop = litTable (nat 1) "iter" natT
 
 
 runGraph :: GraphM AlgRes -> AlgPlan
-runGraph = (\(r, (_,_,m)) -> (m, r) ) . flip runState (1, empty, M.empty) . flip runReaderT ([], initLoop)
+runGraph = (\(r, (_,m)) -> (m, r) ) . flip runState (1, M.empty) . flip runReaderT ([], initLoop)
 
 getLoop :: GraphM AlgNode
 getLoop = do 
@@ -44,13 +34,13 @@ getLoop = do
 
 getFreshId :: GraphM Int
 getFreshId = do
-                (n, s, t) <- get
-                put $ (n + 1, s, t)
+                (n, t) <- get
+                put $ (n + 1, t)
                 return n
 
 findNode :: AlgNode -> GraphM (Maybe Int)
 findNode n = do
-              (_, _, t) <- get
+              (_, t) <- get
               return $ M.lookup n t
               
 insertNode :: AlgNode-> GraphM Int
@@ -64,24 +54,11 @@ insertNode (n, children) = do
 insertNode' :: Algebra -> [Int] -> GraphM Int
 insertNode' n children = do 
                               i <- getFreshId 
-                              (sup, s, t) <- get
-                              let s' = ([], i, n, [((), c) | c <- children]) & s
+                              (sup, t) <- get
                               let t' = M.insert (n, children) i t
-                              put $ (sup, s', t')
+                              put $ (sup, t')
                               return i
 
-insertEdge :: Int -> Int ->  GraphM ()
-insertEdge from to = do
-                      (sup, s, t) <- get
-                      let s' = insEdge (from, to, ()) s
-                      put (sup, s', t)
-
-insertEdges :: Int -> [Int] -> GraphM ()
-insertEdges from (x:xs)  = do
-                                insertEdge from x
-                                insertEdges from xs
-insertEdges _ []           = return ()
-                
 withBinding :: String -> Int -> GraphM a -> GraphM a
 withBinding n v a = do
                      local (\(g, alg) -> ((n, v):g, alg)) a
