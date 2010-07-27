9ac33e89426abe3baf9ac924d173a2b7cd7ff16e
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-07-27 09:46:41 +0200

    A few words of comment

--- Implementation/src/Ferry/TypedCore/Convert/CoreToAlgebra.hs
+++ Implementation/src/Ferry/TypedCore/Convert/CoreToAlgebra.hs
@@ -9,15 +9,22 @@ import Ferry.TypedCore.Data.TypedCore
 
 import qualified Data.Map as M 
 
+
+--Results are stored in column:
 resCol = "item999999001"
 
+--Construct the ith item columns
 mkPrefixCol i = "item" ++ prefixCol ++ (show i)
 
+--Construct the ith iter column
 mkPrefixIter i = "iter" ++ prefixCol ++ (show i)
 
+--Prefix for intermediate column numbers
 prefixCol = "9999"
 
+-- Transform Ferry core into a relation algebra modelled as a DAG
 coreToAlgebra :: CoreExpr -> GraphM AlgRes
+-- Primitive values
 coreToAlgebra (Constant t (CInt i)) = do 
                                         loop <- getLoop
                                         n1 <- insertNode loop
@@ -42,6 +49,7 @@ coreToAlgebra (Constant t (CString i)) = do
                                           n2 <- insertNode $ attach "item1" stringT (string i) n1
                                           n3 <- insertNode $ attach "pos" intT (int 1) n2
                                           return (n3, [Col 1], EmptySub)
+-- Binary operators
 coreToAlgebra (BinOp t (Op o) e1 e2) = do
                                          (q1, [Col 1], m1) <- coreToAlgebra e1
                                          (q2, [Col 1], m2) <- coreToAlgebra e2
@@ -50,11 +58,15 @@ coreToAlgebra (BinOp t (Op o) e1 e2) = do
                                          n3 <- insertNode $ oper o resCol  "item1" (mkPrefixCol 1) n2
                                          n4 <- insertNode $ proj [("iter", "iter"), ("pos", "pos"), ("item1", resCol)] n3
                                          return (n4, [Col 1], EmptySub)
+-- Let bindings
 coreToAlgebra (Let t s e1 e2) = do
                                     (q1, cs1, m1) <- coreToAlgebra e1
                                     withBinding s (q1, cs1, m1) $ coreToAlgebra e2
+-- Variable lookup
 coreToAlgebra (Var t n) = fromGam n
+-- Record construction, body of the rule can be found in recElemsToAlgebra
 coreToAlgebra (Rec t (e:els)) = foldl recElemsToAlgebra (recElemToAlgebra e) els
+-- Record element access.
 coreToAlgebra (Elem t e n) = do
                                 (q1, cs1 ,ts1) <- coreToAlgebra e
                                 let csn = getCol n cs1
@@ -62,12 +74,14 @@ coreToAlgebra (Elem t e n) = do
                                 let projPairs = zip (leafNames csn') (leafNames csn)
                                 n1 <- insertNode $ proj (("iter", "iter"):("pos", "pos"):projPairs) q1
                                 return (n1, csn', EmptySub)
-                             
+
+-- Transform a record element into algebraic plan                             
 recElemToAlgebra :: RecElem -> GraphM AlgRes
 recElemToAlgebra (RecElem t n e) = do
                                      (q1, cs1, ts1) <- coreToAlgebra e
                                      return (q1, [NCol n cs1], ts1)
-                                     
+
+-- Transform a record into an algebraic plan                                     
 recElemsToAlgebra :: GraphM AlgRes -> RecElem -> GraphM AlgRes
 recElemsToAlgebra alg2 el = do
                                 (q1, cs1, ts1) <- alg2
@@ -80,26 +94,34 @@ recElemsToAlgebra alg2 el = do
                                 let projPairs' = zip (leafNames cs1) (leafNames cs1) ++ zip (leafNames cs2') (leafNames cs2')
                                 n3 <- insertNode $ proj (("iter", "iter"):("pos", "pos"):projPairs') n2
                                 return (n3, cs1 ++ cs2', EmptySub)
-                                    
+
+-- Function to transform the column structure
+
+-- Get all the column names from the structure                                    
 leafNames :: Columns -> [String]
 leafNames cs = map (\i -> "item" ++ show i) $ colLeafs cs
 
+-- Get all the leaf columns, that is the columns that are actually a column
 colLeafs :: Columns -> [Int]
 colLeafs ((Col i):xs) = (:) i $ colLeafs xs
 colLeafs ((NCol _ cs):xs) = colLeafs cs ++ colLeafs xs
 colLeafs []               = []
 
+-- Count the number of columns
 colSize :: Columns -> Int
 colSize = length . colLeafs
 
+-- Increment the column numbers by a given amount
 incrCols :: Int -> Columns -> Columns
 incrCols inc ((Col i):xs)    = (Col (i + inc)):(incrCols inc xs)
 incrCols inc ((NCol x i):xs) = (NCol x (incrCols inc i)):(incrCols inc xs)
 incrCols inc []              = [] 
 
+-- Find the lowest column number
 minCol :: Columns -> Int
 minCol c = minimum $ (:) 1 $ colLeafs c
 
+-- Decrement the column numbers so that the lowest column number is 1 after applying
 decrCols :: Columns -> Columns
 decrCols cols = let minV = minCol cols
                  in decr' (minV - 1) cols
@@ -109,6 +131,7 @@ decrCols cols = let minV = minCol cols
      decr' decr ((NCol x i):xs) = (NCol x $ decr' decr i) : (decr' decr xs)
      decr' _    []              = []
 
+-- Find the columns associated with a record label
 getCol :: String -> Columns -> Columns
 getCol n cs = getCol' cs
     where
