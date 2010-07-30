3e4942f257368ed6c92ea4918793afc2ec9e94bc
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-07-30 12:01:30 +0200

    Removed that very same pattern here as well

--- Implementation/src/Ferry/Core/Render/Dot.hs
+++ Implementation/src/Ferry/Core/Render/Dot.hs
@@ -18,80 +18,80 @@ toDot (BinOp o e1 e2) = do
                           id1 <- toDot e1
                           id2 <- toDot e2
                           let o' = (\(Op o) -> o) o
-                          addNode $ Node nId [Label $ SLabel o', Color Green, Shape Circle]
-                          addEdge $ Edge nId [id1, id2]
+                          node nId [Label $ SLabel o', Color Green, Shape Circle]
+                          edge nId [id1, id2]
                           return nId
 {- toDot (UnaOp o e) = do
                       nId <- getFreshId
                       eId <- toDot e
                       let o' = (\(Op o) -> o) o
-                      addNode $ Node nId [Label $ SLabel o', Color Green, Shape Circle]
-                      addEdge $ Edge nId [eId]
+                      node nId [Label $ SLabel o', Color Green, Shape Circle]
+                      edge nId [eId]
                       return nId -}
 toDot (Constant c) = do
                       nId <- getFreshId
                       let s = toString c
-                      addNode $ Node nId [Label $ SLabel s, Color Yellow, Shape Triangle]
+                      node nId [Label $ SLabel s, Color Yellow, Shape Triangle]
                       return nId
 toDot (Var i) = do
                     nId <- getFreshId
-                    addNode $ Node nId [Label $ SLabel i, Color Red, Shape Triangle]
+                    node nId [Label $ SLabel i, Color Red, Shape Triangle]
                     return nId
 toDot (App c ps) = do
                      nId <- getFreshId
                      fId <- toDot c
                      pIds <- paramToDot ps
-                     addNode $ Node nId [Label $ SLabel "$", Color Green, Shape Circle]
-                     addEdge $ Edge nId [fId, pIds]
+                     node nId [Label $ SLabel "$", Color Green, Shape Circle]
+                     edge nId [fId, pIds]
                      return nId
 toDot (Let s e1 e2) = do
                        nId <- getFreshId
                        id0 <- getFreshId
                        id1 <- toDot e1
                        id2 <- toDot e2
-                       addNode $ Node id0 [Label $ SLabel s, Color Red, Shape Rect, TextColor White]
-                       addNode $ Node nId [Label $ SLabel "Let", Color Blue, Shape Rect]
-                       addEdge $ Edge nId [id0, id1, id2]
+                       node id0 [Label $ SLabel s, Color Red, Shape Rect, TextColor White]
+                       node nId [Label $ SLabel "Let", Color Blue, Shape Rect]
+                       edge nId [id0, id1, id2]
                        return nId
 toDot (Rec es) = do
                   nId <- getFreshId
                   eIds <- mapM recToDot es
-                  addNode $ Node nId [Label $ SLabel "Rec", Color Blue, Shape Oval]
-                  addEdge $ Edge nId eIds
+                  node nId [Label $ SLabel "Rec", Color Blue, Shape Oval]
+                  edge nId eIds
                   return nId
 toDot (Cons e1 e2) = do
                      nId <- getFreshId
                      eIdh <- toDot e1
                      eIdt <- toDot e2
-                     addNode $ Node nId [Label $ SLabel "Cons", Color Blue, Shape Oval]
-                     addEdge $ Edge nId [eIdh, eIdt]
+                     node nId [Label $ SLabel "Cons", Color Blue, Shape Oval]
+                     edge nId [eIdh, eIdt]
                      return nId
 toDot (Nil)      = do
                     nId <- getFreshId
-                    addNode $ Node nId [Label $ SLabel "Nil", Color Blue, Shape Oval]
+                    node nId [Label $ SLabel "Nil", Color Blue, Shape Oval]
                     return nId
 toDot (Elem c s) = do
                     nId <- getFreshId
                     sId <- getFreshId
-                    addNode $ Node nId [Label $ SLabel ".", Color Green, Shape Circle]
-                    addNode $ Node sId [Label $ SLabel s, Color Red, Shape Triangle]
+                    node nId [Label $ SLabel ".", Color Green, Shape Circle]
+                    node sId [Label $ SLabel s, Color Red, Shape Triangle]
                     cId <- toDot c
-                    addEdge $ Edge nId [cId, sId]
+                    edge nId [cId, sId]
                     return nId
 toDot (Table n cs ks) = do
                          nId <- getFreshId
                          let label = VLabel $ ((HLabel [SLabel "Table:", SLabel n])
                                             : [HLabel [SLabel $ n ++ "::", SLabel $ prettyTy t ] | (Column n t) <- cs])
                                             ++ [SLabel $ keyToString k | k <- ks]
-                         addNode $ Node nId [Shape Rect, Label label, Color Yellow]
+                         node nId [Shape Rect, Label label, Color Yellow]
                          return nId
 toDot (If e1 e2 e3) = do
                         nId <- getFreshId
                         eId1 <- toDot e1
                         eId2 <- toDot e2
                         eId3 <- toDot e3
-                        addNode $ Node nId [Label $ SLabel "If", Color Blue, Shape Circle]
-                        addEdge $ Edge nId [eId1, eId2, eId2]
+                        node nId [Label $ SLabel "If", Color Blue, Shape Circle]
+                        edge nId [eId1, eId2, eId2]
                         return nId
                         
 
@@ -101,25 +101,25 @@ paramToDot (ParAbstr p e) = do
                              nId <- getFreshId
                              pId <- patToDot p
                              eId <- toDot e
-                             addNode $ Node nId [Label $ SLabel "\\   ->", Color Blue, Shape Circle]
-                             addEdge $ Edge nId [pId, eId]
+                             node nId [Label $ SLabel "\\   ->", Color Blue, Shape Circle]
+                             edge nId [pId, eId]
                              return nId
                              
 patToDot :: Pattern -> Dot Id
 patToDot (PVar s) = do
                      nId <- getFreshId
-                     addNode $ Node nId [Label $ SLabel s, Color Red, Shape Triangle]
+                     node nId [Label $ SLabel s, Color Red, Shape Triangle]
                      return nId
 patToDot (Pattern s) = do
                         nId <- getFreshId
-                        addNode $ Node nId [Label $ SLabel $  "(" ++ (concat $ L.intersperse ", " s) ++ ")", Color Red, Shape Triangle]
+                        node nId [Label $ SLabel $  "(" ++ (concat $ L.intersperse ", " s) ++ ")", Color Red, Shape Triangle]
                         return nId
                         
 recToDot (RecElem s e) = do
                           nId <- getFreshId
                           eId <- toDot e
-                          addNode $ Node nId [Label $ SLabel s, Color Red, Shape Oval]
-                          addEdge $ Edge nId [eId]
+                          node nId [Label $ SLabel s, Color Red, Shape Oval]
+                          edge nId [eId]
                           return nId
 
 keyToString :: Key -> String
