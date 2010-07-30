fd3848535d96f762effbe885353e173639d7b4eb
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-07-30 15:28:47 +0200

    Dot code generating a bit more elegant

--- Implementation/src/Ferry/Core/Render/Dot.hs
+++ Implementation/src/Ferry/Core/Render/Dot.hs
@@ -27,69 +27,49 @@ toDot (BinOp o e1 e2) = do
                       node nId [Label $ SLabel o', Color Green, Shape Circle]
                       edge nId [eId]
                       return nId -}
-toDot (Constant c) = do
-                      nId <- getFreshId
-                      let s = toString c
-                      node nId [Label $ SLabel s, Color Yellow, Shape Triangle]
-                      return nId
-toDot (Var i) = do
-                    nId <- getFreshId
-                    node nId [Label $ SLabel i, Color Red, Shape Triangle]
-                    return nId
+toDot (Constant c) = let s = toString c
+                      in node [Label $ SLabel s, Color Yellow, Shape Triangle]
+toDot (Var i) = node [Label $ SLabel i, Color Red, Shape Triangle]
 toDot (App c ps) = do
-                     nId <- getFreshId
+                     nId <- node [Label $ SLabel "$", Color Green, Shape Circle]
                      fId <- toDot c
                      pIds <- paramToDot ps
-                     node nId [Label $ SLabel "$", Color Green, Shape Circle]
                      edge nId [fId, pIds]
                      return nId
 toDot (Let s e1 e2) = do
-                       nId <- getFreshId
-                       id0 <- getFreshId
+                       nId <- node [Label $ SLabel "Let", Color Blue, Shape Rect]
+                       id0 <- node [Label $ SLabel s, Color Red, Shape Rect, TextColor White]
                        id1 <- toDot e1
                        id2 <- toDot e2
-                       node id0 [Label $ SLabel s, Color Red, Shape Rect, TextColor White]
-                       node nId [Label $ SLabel "Let", Color Blue, Shape Rect]
                        edge nId [id0, id1, id2]
                        return nId
 toDot (Rec es) = do
-                  nId <- getFreshId
+                  nId <- node [Label $ SLabel "Rec", Color Blue, Shape Oval]
                   eIds <- mapM recToDot es
-                  node nId [Label $ SLabel "Rec", Color Blue, Shape Oval]
                   edge nId eIds
                   return nId
 toDot (Cons e1 e2) = do
-                     nId <- getFreshId
+                     nId <- node [Label $ SLabel "Cons", Color Blue, Shape Oval]
                      eIdh <- toDot e1
                      eIdt <- toDot e2
-                     node nId [Label $ SLabel "Cons", Color Blue, Shape Oval]
                      edge nId [eIdh, eIdt]
                      return nId
-toDot (Nil)      = do
-                    nId <- getFreshId
-                    node nId [Label $ SLabel "Nil", Color Blue, Shape Oval]
-                    return nId
+toDot (Nil)      = node [Label $ SLabel "Nil", Color Blue, Shape Oval]
 toDot (Elem c s) = do
-                    nId <- getFreshId
-                    sId <- getFreshId
-                    node nId [Label $ SLabel ".", Color Green, Shape Circle]
-                    node sId [Label $ SLabel s, Color Red, Shape Triangle]
+                    nId <- node [Label $ SLabel ".", Color Green, Shape Circle]
+                    sId <- node [Label $ SLabel s, Color Red, Shape Triangle]
                     cId <- toDot c
                     edge nId [cId, sId]
                     return nId
-toDot (Table n cs ks) = do
-                         nId <- getFreshId
-                         let label = VLabel $ ((HLabel [SLabel "Table:", SLabel n])
+toDot (Table n cs ks) = let label = VLabel $ ((HLabel [SLabel "Table:", SLabel n])
                                             : [HLabel [SLabel $ n ++ "::", SLabel $ prettyTy t ] | (Column n t) <- cs])
                                             ++ [SLabel $ keyToString k | k <- ks]
-                         node nId [Shape Rect, Label label, Color Yellow]
-                         return nId
+                         in node [Shape Rect, Label label, Color Yellow]
 toDot (If e1 e2 e3) = do
-                        nId <- getFreshId
+                        nId <- node [Label $ SLabel "If", Color Blue, Shape Circle]
                         eId1 <- toDot e1
                         eId2 <- toDot e2
                         eId3 <- toDot e3
-                        node nId [Label $ SLabel "If", Color Blue, Shape Circle]
                         edge nId [eId1, eId2, eId2]
                         return nId
                         
@@ -97,27 +77,18 @@ toDot (If e1 e2 e3) = do
 paramToDot :: Param -> Dot Id
 paramToDot (ParExpr e) = toDot e
 paramToDot (ParAbstr p e) = do
-                             nId <- getFreshId
+                             nId <- node [Label $ SLabel "\\   ->", Color Blue, Shape Circle]
                              pId <- patToDot p
                              eId <- toDot e
-                             node nId [Label $ SLabel "\\   ->", Color Blue, Shape Circle]
                              edge nId [pId, eId]
                              return nId
                              
 patToDot :: Pattern -> Dot Id
-patToDot (PVar s) = do
-                     nId <- getFreshId
-                     node nId [Label $ SLabel s, Color Red, Shape Triangle]
-                     return nId
-patToDot (Pattern s) = do
-                        nId <- getFreshId
-                        node nId [Label $ SLabel $  "(" ++ (concat $ L.intersperse ", " s) ++ ")", Color Red, Shape Triangle]
-                        return nId
-                        
+patToDot (PVar s) = node [Label $ SLabel s, Color Red, Shape Triangle]
+patToDot (Pattern s) = node [Label $ SLabel $  "(" ++ (concat $ L.intersperse ", " s) ++ ")", Color Red, Shape Triangle]
 recToDot (RecElem s e) = do
-                          nId <- getFreshId
+                          nId <- node [Label $ SLabel s, Color Red, Shape Oval]
                           eId <- toDot e
-                          node nId [Label $ SLabel s, Color Red, Shape Oval]
                           edge nId [eId]
                           return nId
 
--- Implementation/src/Ferry/TypedCore/Render/Dot.hs
+++ Implementation/src/Ferry/TypedCore/Render/Dot.hs
@@ -16,14 +16,13 @@ import qualified Data.List as L
 
 toDot :: CoreExpr -> Dot Id
 toDot (BinOp t o e1 e2) = do
-                          nId <- getFreshId
-                          tId <- typeToDot t
-                          id1 <- toDot e1
-                          id2 <- toDot e2
-                          let o' = (\(Op o) -> o) o
-                          node nId [Label $ SLabel o', Color Green, Shape Circle]
-                          edge nId [id1, id2, tId]
-                          return nId
+                           let o' = (\(Op o) -> o) o  
+                           nId <- node [Label $ SLabel o', Color Green, Shape Circle]
+                           tId <- typeToDot t
+                           id1 <- toDot e1
+                           id2 <- toDot e2
+                           edge nId [id1, id2, tId]
+                           return nId
 {- toDot (UnaOp t o e) = do
                       nId <- getFreshId
                       tId <- typeToDot t
@@ -33,82 +32,70 @@ toDot (BinOp t o e1 e2) = do
                       edge nId [eId, tId]
                       return nId -}
 toDot (Constant t c) = do
-                      nId <- getFreshId
-                      tId <- typeToDot t
                       let s = toString c
-                      node nId [Label $ SLabel s, Color Yellow, Shape Triangle]
+                      nId <- node [Label $ SLabel s, Color Yellow, Shape Triangle]
+                      tId <- typeToDot t
                       edge nId [tId]
                       return nId
 toDot (Var t i) = do
-                    nId <- getFreshId
+                    nId <- node [Label $ SLabel i, Color Red, Shape Triangle]
                     tId <- typeToDot t
                     edge nId [tId]
-                    node nId [Label $ SLabel i, Color Red, Shape Triangle]
                     return nId
 toDot (App t c ps) = do
-                     nId <- getFreshId
+                     nId <- node [Label $ SLabel "$", Color Green, Shape Circle]
                      tId <- typeToDot t
                      fId <- toDot c
                      pIds <- paramToDot ps
-                     node nId [Label $ SLabel "$", Color Green, Shape Circle]
                      edge nId [fId, pIds, tId]
                      return nId
 toDot (Let t s e1 e2) = do
-                       nId <- getFreshId
+                       nId <- node [Label $ SLabel "Let", Color Blue, Shape Rect]
                        tId <- typeToDot t
-                       id0 <- getFreshId
+                       id0 <- node [Label $ SLabel s, Color Red, Shape Rect, TextColor White]
                        id1 <- toDot e1
                        id2 <- toDot e2
-                       node id0 [Label $ SLabel s, Color Red, Shape Rect, TextColor White]
-                       node nId [Label $ SLabel "Let", Color Blue, Shape Rect]
                        edge nId [id0, id1, id2, tId]
                        return nId
 toDot (Rec t es) = do
-                  nId <- getFreshId
+                  nId <- node [Label $ SLabel "Rec", Color Blue, Shape Oval]
                   tId <- typeToDot t
                   eIds <- mapM recToDot es
-                  node nId [Label $ SLabel "Rec", Color Blue, Shape Oval]
                   edge nId (eIds ++ [tId])
                   return nId
 toDot (Cons t e1 e2) = do
-                     nId <- getFreshId
+                     nId <- node [Label $ SLabel "Cons", Color Blue, Shape Oval]
                      tId <- typeToDot t
                      eIdh <- toDot e1
                      eIdt <- toDot e2
-                     node nId [Label $ SLabel "Cons", Color Blue, Shape Oval]
                      edge nId [eIdh, eIdt, tId]
                      return nId
 toDot (Nil t)      = do
-                    nId <- getFreshId
+                    nId <- node [Label $ SLabel "Nil", Color Blue, Shape Oval]
                     tId <- typeToDot t
-                    node nId [Label $ SLabel "Nil", Color Blue, Shape Oval]
                     edge nId [tId]
                     return nId
 toDot (Elem t c s) = do
-                    nId <- getFreshId
-                    sId <- getFreshId
+                    nId <- node [Label $ SLabel ".", Color Green, Shape Circle]
+                    sId <- node [Label $ SLabel s, Color Red, Shape Triangle]
                     tId <- typeToDot t
-                    node nId [Label $ SLabel ".", Color Green, Shape Circle]
-                    node sId [Label $ SLabel s, Color Red, Shape Triangle]
                     cId <- toDot c
                     edge nId [cId, sId, tId]
                     return nId
 toDot (Table ty n cs ks) = do
-                         nId <- getFreshId
-                         tId <- typeToDot ty
                          let label = VLabel $ ((HLabel [SLabel "Table:", SLabel n])
                                             : [HLabel [SLabel $ n ++ "::", SLabel $ prettyPrint t ] | (Column n t) <- cs])
                                             ++ [SLabel $ keyToString k | k <- ks]
-                         node nId [Shape Rect, Label label, Color Yellow]
+                         nId <- node [Shape Rect, Label label, Color Yellow]
+                         tId <- typeToDot ty
                          edge nId [tId]
                          return nId
 toDot (If t e1 e2 e3) = do
-                        nId <- getFreshId
+                        nId <- node [Label $ SLabel "If", Color Blue, Shape Circle]
                         tId <- typeToDot t
                         eId1 <- toDot e1
                         eId2 <- toDot e2
                         eId3 <- toDot e3
-                        node nId [Label $ SLabel "If", Color Blue, Shape Circle]
                         edge nId [eId1, eId2, eId3, tId]
                         return nId
                         
@@ -116,29 +103,21 @@ toDot (If t e1 e2 e3) = do
 paramToDot :: Param -> Dot Id
 paramToDot (ParExpr t e) = toDot e
 paramToDot (ParAbstr t p e) = do
-                             nId <- getFreshId
+                             nId <- node [Label $ SLabel "\\   ->", Color Blue, Shape Circle]
                              tId <- typeToDot t
                              pId <- patToDot p
                              eId <- toDot e
-                             node nId [Label $ SLabel "\\   ->", Color Blue, Shape Circle]
                              edge nId [pId, eId, tId]
                              return nId
                              
 patToDot :: Pattern -> Dot Id
-patToDot (PVar s) = do
-                     nId <- getFreshId
-                     node nId [Label $ SLabel s, Color Red, Shape Triangle]
-                     return nId
-patToDot (Pattern s) = do
-                        nId <- getFreshId
-                        node nId [Label $ SLabel $  "(" ++ (concat $ L.intersperse ", " s) ++ ")", Color Red, Shape Triangle]
-                        return nId
+patToDot (PVar s) = node [Label $ SLabel s, Color Red, Shape Triangle]
+patToDot (Pattern s) = node [Label $ SLabel $  "(" ++ (concat $ L.intersperse ", " s) ++ ")", Color Red, Shape Triangle]
                         
 recToDot (RecElem t s e) = do
-                          nId <- getFreshId
+                          nId <- node [Label $ SLabel s, Color Red, Shape Oval]
                           tId <- typeToDot t
                           eId <- toDot e
-                          node nId [Label $ SLabel s, Color Red, Shape Oval]
                           edge nId [eId, tId]
                           return nId
 
@@ -146,7 +125,4 @@ keyToString :: Key -> String
 keyToString (Key ks) = "(" ++ (concat $ L.intersperse ", " ks) ++ ")"
 
 typeToDot :: Qual FType -> Dot Id
-typeToDot t = do
-               nId <- getFreshId
-               node nId [Label $ SLabel $ prettyPrint t, Color Gray, Shape Rect]
-               return nId
+typeToDot t = node [Label $ SLabel $ prettyPrint t, Color Gray, Shape Rect]
