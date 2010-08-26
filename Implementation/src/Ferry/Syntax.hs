5e81cacd72ef569525fca861b5c0e90a48a9ec80
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-08-26 08:07:01 +0200

    Expose the hasType class, convenient for constructing FerryCore ASTs. I can haz type plz?

--- Implementation/src/Ferry/Syntax.hs
+++ Implementation/src/Ferry/Syntax.hs
@@ -3,11 +3,12 @@ module Ferry.Syntax
 Ident,
 Identifier, Const (..),
 Op (..), CoreExpr (..), RecElem (..), Param (..), Pattern (..), Column (..), Key (..),
-int, float, string, bool, list, var, rec, fn, genT, (.->), TyScheme (..), Qual (..), Pred (..), FType (..), RLabel (..), FTFn (..)
+int, float, string, bool, list, var, rec, fn, genT, (.->), TyScheme (..), Qual (..), Pred (..), FType (..), RLabel (..), FTFn (..), HasType, typeOf
 )
 where
     
 import Ferry.TypedCore.Data.Base (Ident)
 import Ferry.Front.Data.Base (Identifier, Const (..))
 import Ferry.TypedCore.Data.TypedCore (Op (..), CoreExpr (..), RecElem (..), Param (..), Pattern (..), Column (..), Key (..))
-import Ferry.TypedCore.Data.Type (int, float, string, bool, list, var, rec, fn, genT, (.->), TyScheme (..), Qual (..), Pred (..), FType (..), RLabel (..), FTFn (..))
+import Ferry.TypedCore.Data.Type (int, float, string, bool, list, var, rec, fn, genT, (.->), TyScheme (..), Qual (..), Pred (..), FType (..), RLabel (..), FTFn (..), HasType, typeOf)
+import Ferry.TypedCore.Data.Instances 
