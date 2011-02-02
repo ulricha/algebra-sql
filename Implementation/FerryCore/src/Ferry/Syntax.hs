ee9bb30e3d3aefa3a322eb9bc2b228b48a048f85
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2011-02-02 14:04:08 +0100

    Fixed a few minor issues. Deprecated the use of untyped syntax

--- Implementation/FerryCore/src/Ferry/Syntax.hs
+++ Implementation/FerryCore/src/Ferry/Syntax.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports   #-}
 {-| Everything related to untyped core -}
-module Ferry.Syntax
+module Ferry.Syntax {-# DEPRECATED "Use Ferry.SyntaxTyped instead of this module. This module will not be available in future FerryCore releases." #-}
 (
 Ident,
 Identifier, Const (..), VarContainer(..),
