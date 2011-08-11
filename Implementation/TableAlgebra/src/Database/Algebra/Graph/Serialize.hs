9916ed38e22d9dff593c985076189217398bae2a
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2011-08-11 17:13:28 +0200

    graph "serialization" using Show/Read and two very basic tools for dot and X100 code generation (codegen, dotgen).

---
+++ Implementation/TableAlgebra/src/Database/Algebra/Graph/Serialize.hs
@@ -0,0 +1,25 @@
+module Database.Algebra.Graph.Serialize where
+
+import Control.Monad
+
+import Data.Map as Map
+
+import Database.Algebra.Graph.GraphBuilder
+
+tagsFromFile :: FilePath -> IO Tags
+tagsFromFile = liftM deserializeTags . readFile
+
+tagsToFile :: FilePath -> Tags -> IO ()
+tagsToFile f ts = writeFile (f ++ ".tags") $ serializeTags ts
+
+serializeTags :: Tags -> String
+serializeTags = show . Map.toList
+
+deserializeTags :: String -> Tags
+deserializeTags s = Map.fromList (read s :: [(AlgNode, [String])])
+
+rootsFromFile :: FilePath -> IO [AlgNode]
+rootsFromFile = liftM (\s -> read s :: [AlgNode]) . readFile
+
+rootsToFile :: FilePath -> [AlgNode] -> IO ()
+rootsToFile f ns = writeFile (f ++ ".roots") $ show ns
