82f2dc694ab45f2663e47e0bb5f5ab99192fd069
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-03-07 10:02:35 +0100

    syntactic experiments for rewrite rules.

---
+++ Implementation/TableAlgebra/src/Database/Algebra/Dag/Playground.hs
@@ -0,0 +1,29 @@
+type Rule a = Match a (DagRewrite a ())
+              
+match :: Operator a => AlgNode -> (a -> Match a b) -> Match a b
+match = undefined
+        
+predicate :: Bool -> Match a ()
+predicate = undefined
+            
+constAggrPattern :: AlgNode -> Rule X100Algebra
+constAggrPattern n = do
+  match n (\op -> case op of 
+              UnOp (Aggr (keys, [])) c -> return (c, keys) 
+              _ -> fail )
+  predicate $ not $ M.null constCols
+  let action = do
+        logRewrite "foobar"
+        replaceM $ n 
+  return action
+  
+pushMergeRule :: AlgNode -> Rule X100Algebra
+pushMergeRule q = do
+  ((q1, q2), sem) <- match q [|p| BinOp (MergeJoin1 sem) q1 q2 -> return ((q1, q2), sem) |]
+  (c, ps) <- match q1 [|p| UnOp (Project ps) c -> return (c, q3) |]
+  predicate $ b == joinCol
+  return $ do
+    logRewrite "Pushdown.MergeJoin1"
+    joinNode' <- insertM $ BinOp (MergeJoin1 sem) q3 q2
+    projectNode' <- insertM $ UnOp (Project ps) joinNode'
+    relinkParentsM q projectNode'
