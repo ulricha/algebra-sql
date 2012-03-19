69df5c808e91035d15bb5b2d455d3d65a6bfa87d
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2012-03-19 11:31:19 +0100

    Module cleanup, documentation, warnings fixed.

--- Implementation/TableAlgebra/src/Database/Algebra/Aux.hs
+++ Implementation/TableAlgebra/src/Database/Algebra/Aux.hs
@@ -1,9 +1,15 @@
+-- | This module provides some very general helper functions.
 module Database.Algebra.Aux where
 
 import qualified Data.Map as M
 
+-- | Perform a map lookup and fail with the given error string if the key
+-- is not present
 lookupUnsafe :: (Ord k, Show k, Show a) => M.Map k a -> String -> k -> a
 lookupUnsafe m s u = 
     case M.lookup u m of
         Just p -> p
         Nothing -> error $ s ++ " " ++ (show u) ++ " in " ++ (show m)
+
+replace :: Eq a => a -> a -> a -> a
+replace orig new x = if x == orig then new else x
--- Implementation/TableAlgebra/src/Database/Algebra/Dag/Common.hs
+++ Implementation/TableAlgebra/src/Database/Algebra/Dag/Common.hs
@@ -2,11 +2,17 @@ module Database.Algebra.Dag.Common where
 
 import qualified Data.Map as M
 
--- FIXME this should be abstract
+-- | Identifiers for DAG nodes.
 type AlgNode = Int 
 
 type AlgMap alg = M.Map alg AlgNode
 type NodeMap a = M.Map AlgNode a
 
 type Tag = String
+
+-- | Binary, unary and leaf nodes of a relational algebra DAG.
+data Algebra b u n c = BinOp b c c
+                     | UnOp u c
+                     | NullaryOp n
+                       deriving (Ord, Eq, Show, Read)
 
