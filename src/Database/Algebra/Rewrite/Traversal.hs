066261a7e7ea405ee5af4647a5e844af9ca7cb3b
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-04-24 11:06:13 +0200

    Re-Infer properties after every applied rewrite during a post-order traversal.
    This is the safe and inefficient choice.

--- src/Database/Algebra/Rewrite/Traversal.hs
+++ src/Database/Algebra/Rewrite/Traversal.hs
@@ -3,11 +3,14 @@ module Database.Algebra.Rewrite.Traversal
        , postOrder
        , iteratively ) where
 
+import Control.Monad
+
 import Database.Algebra.Dag
 import Database.Algebra.Dag.Common
 import Database.Algebra.Rewrite.DagRewrite
 import Database.Algebra.Rewrite.Rule
 
+-- FIXME fix property inference for preorder traversal
 -- | Infer properties, then traverse the DAG in preorder fashion and apply the rule set 
 -- at every node.
 preOrder :: Operator o => DagRewrite o (NodeMap p) -> RuleSet o p -> DagRewrite o Bool
@@ -28,17 +31,30 @@ preOrder infer rules =
 -- every node.
 postOrder :: Operator o => DagRewrite o (NodeMap p) -> RuleSet o p -> DagRewrite o Bool
 postOrder infer rules = 
-  let traverse props q = do
+  let traverse (changedPrev, props) q = do
         op <- operatorM q
         let cs = opChildren op
-        changedChild <- mapM (\c -> traverse props c) cs
-        changedSelf <- applyRuleSet props rules q
-        return $ changedSelf || (or changedChild)
+        (changedChild, mProps) <- foldM descend (False, props) cs
+        props' <- case mProps of
+              Just ps -> return ps
+              Nothing -> infer
+        
+        changedSelf <- applyRuleSet props' rules q
+        if changedSelf
+          then return (True, Nothing)
+          else return (changedChild || changedPrev, Just props')
+      
+      descend (changedPrev, mProps) c = do
+          props <- case mProps of
+            Just ps -> return ps
+            Nothing -> infer
+          traverse (changedPrev, Just props) c
+        
   in do
     pm <- infer
     rs <- rootNodesM
-    changed <- mapM (\r -> traverse pm r) rs
-    return $ or changed
+    (changed, _) <- foldM traverse (False, Just pm) rs
+    return changed
   
 -- | Iteratively apply a rewrite, until no further changes occur.
 iteratively :: DagRewrite o Bool -> DagRewrite o Bool
