30972eb77d33ac185d8ea26d9a81b2ebce6966e0
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-07-05 17:06:08 +0200

    JSON deserialization for VL plans.

--- src/Database/Algebra/VL/Render/JSON.hs
+++ src/Database/Algebra/VL/Render/JSON.hs
@@ -1,13 +1,16 @@
-module Database.Algebra.VL.Render.JSON where
+{-# LANGUAGE DeriveGeneric #-}
 
-import Database.Algebra.Dag.Common
-  
-import Database.Algebra.VL.Data
+module Database.Algebra.VL.Render.JSON(serializePlan, deserializePlan, planToFile, planFromFile) where
 
+import GHC.Generics(Generic)
 import qualified Data.Map as M
+import Control.Monad
 
 import qualified Data.ByteString.Lazy.Char8 as BL
-import Data.Aeson (ToJSON, encode)
+import Data.Aeson (ToJSON, FromJSON, encode, decode)
+
+import Database.Algebra.Dag.Common
+import Database.Algebra.VL.Data
 
 instance ToJSON TerOp where
 instance ToJSON BinOp where
@@ -18,10 +21,32 @@ instance ToJSON VLType where
 instance ToJSON VecOp where
 instance (ToJSON t, ToJSON b, ToJSON u, ToJSON n, ToJSON c) => ToJSON (Algebra t b u n c) where
     
-renderVLJSON :: NodeMap [Tag] -> [AlgNode] -> NodeMap VL -> String
-renderVLJSON ts rootNodes nodeMap = BL.unpack $ encode (M.toList ts, rootNodes, M.toList nodeMap)
+instance FromJSON TerOp where
+instance FromJSON BinOp where
+instance FromJSON UnOp where
+instance FromJSON NullOp where
+instance FromJSON VLVal where
+instance FromJSON VLType where
+instance FromJSON VecOp where
+instance (FromJSON t, FromJSON b, FromJSON u, FromJSON n, FromJSON c) => FromJSON (Algebra t b u n c) where
 
+instance ToJSON Plan where
+instance FromJSON Plan where
+   
+data Plan = Plan {tags :: [(AlgNode, [Tag])], roots :: [AlgNode], graph :: [(AlgNode, VL)]}
+    deriving Generic
+             
+serializePlan :: (NodeMap [Tag], [AlgNode], NodeMap VL) -> BL.ByteString
+serializePlan (ts, rs, g) = let tags' = M.toList ts
+                                graph' = M.toList g
+                             in encode $ Plan {tags = tags', roots = rs, graph = graph'}
 
-
+deserializePlan :: BL.ByteString -> (NodeMap [Tag], [AlgNode], NodeMap VL)
+deserializePlan s = let Just (Plan ts rs g) = decode s
+                    in (M.fromList ts, rs, M.fromList g)
     
-    
+planToFile :: FilePath -> (NodeMap [Tag], [AlgNode], NodeMap VL) -> IO ()
+planToFile f t = BL.writeFile f $ serializePlan t
+
+planFromFile :: FilePath -> IO (NodeMap [Tag], [AlgNode], NodeMap VL)
+planFromFile f = liftM deserializePlan $ BL.readFile f
