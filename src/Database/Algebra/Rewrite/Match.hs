4c63dc1f4d3809d3acbfbafdedbab48f3868ce03
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-11-05 16:41:11 +0100

    Ugly hack: the Match monad's environment features a polymorphic field to store any additional information (e.g. Shape in VL rewrites). This is to ensure that we don't need to abstract over the match monads, which causes headaches in PatternConstruction w.r.t. hole-patterns.

--- src/Database/Algebra/Rewrite/Match.hs
+++ src/Database/Algebra/Rewrite/Match.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies  #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Database.Algebra.Rewrite.Match 
-       ( DefaultMatch(..)
-       , runDefaultMatch
+       ( Match(..)
+       , runMatch
        , getParents
        , getOperator
        , hasPath
@@ -10,6 +10,8 @@ module Database.Algebra.Rewrite.Match
        , predicate
        , try
        , matchOp
+       , lookupExtras
+       , exposeEnv
        , properties) where
 
 import qualified Data.Map as M
@@ -21,52 +23,59 @@ import Control.Applicative
 import Database.Algebra.Dag.Common
 import qualified Database.Algebra.Dag as Dag
   
-data Env o p = Env { dag :: Dag.AlgebraDag o
-                   , propMap :: NodeMap p }
+data Env o p e = Env { dag :: Dag.AlgebraDag o
+                     , propMap :: NodeMap p
+                     , extras :: e }
 
 -- | The Match monad models the failing of a match and provides limited read-only access
 -- to the DAG.
-newtype DefaultMatch o p a = M (MaybeT (Reader (Env o p)) a) deriving (Monad, Functor, Applicative)
+newtype Match o p e a = M (MaybeT (Reader (Env o p e)) a) deriving (Monad, Functor, Applicative)
                                                                       
 -- | Runs a match on the supplied DAG. If the Match fails, 'Nothing' is returned.
 -- If the Match succeeds, it returns just the result.
-runDefaultMatch :: Dag.AlgebraDag o -> NodeMap p -> DefaultMatch o p a -> Maybe a
-runDefaultMatch d pm (M match) = runReader (runMaybeT match) env
-  where env = Env { dag = d, propMap = pm }
+runMatch :: e -> Dag.AlgebraDag o -> NodeMap p -> Match o p e a -> Maybe a
+runMatch e d pm (M match) = runReader (runMaybeT match) env
+  where env = Env { dag = d, propMap = pm, extras = e }
      
--- | Returns the parents of a node in a DefaultMatch context.
-getParents :: AlgNode -> DefaultMatch o p [AlgNode]
+-- | Returns the parents of a node in a Match context.
+getParents :: AlgNode -> Match o p e [AlgNode]
 getParents q = M $ asks ((Dag.parents q) . dag)
           
-getOperator :: AlgNode -> DefaultMatch o p o
+getOperator :: AlgNode -> Match o p e o
 getOperator q = M $ asks ((Dag.operator q) . dag)
 
-hasPath :: AlgNode -> AlgNode -> DefaultMatch o p Bool
+hasPath :: AlgNode -> AlgNode -> Match o p e Bool
 hasPath q1 q2 = M $ asks ((Dag.hasPath q1 q2) . dag)
               
-getRootNodes :: DefaultMatch o p [AlgNode]
+getRootNodes :: Match o p e [AlgNode]
 getRootNodes = M $ asks (Dag.rootNodes . dag)
 
 -- | Fails the complete match if the predicate is False.
-predicate :: Bool -> DefaultMatch o p ()
+predicate :: Bool -> Match o p e ()
 predicate True    = M $ return ()
 predicate False   = M $ fail ""
              
 -- | Fails the complete match if the value is Nothing
-try :: Maybe a -> DefaultMatch o p a
+try :: Maybe a -> Match o p e a
 try (Just x) = return x
 try Nothing  = fail ""
                   
 -- | Runs the supplied Match action on the operator that belongs to the given node.
-matchOp :: AlgNode -> (o -> DefaultMatch o p a) -> DefaultMatch o p a
+matchOp :: AlgNode -> (o -> Match o p e a) -> Match o p e a
 matchOp q match = M $ asks ((Dag.operator q) . dag) >>= (\o -> unwrap $ match o)
   where unwrap (M r) = r
 
 -- | Look up the properties for a given node.
-properties :: AlgNode -> DefaultMatch o p p
+properties :: AlgNode -> Match o p e p
 properties q = do
   M $ do 
     pm <- asks propMap
     case M.lookup q pm of
       Just p -> return p
-      Nothing -> error $ "DefaultMatch.properties: no properties for node " ++ (show q)
+      Nothing -> error $ "Match.properties: no properties for node " ++ (show q)
+  
+lookupExtras :: Match o p e e
+lookupExtras = M $ asks extras
+
+exposeEnv :: Match o p e (Env o p e)
+exposeEnv = M $ ask
