7642a6d2ca0e9a1f5c532194fb4a7abfc2ab435a
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-11-05 18:03:04 +0100

    Include Rewrite.Match in convenience module Rewrite.

--- src/Database/Algebra/Rewrite.hs
+++ src/Database/Algebra/Rewrite.hs
@@ -3,9 +3,15 @@ module Database.Algebra.Rewrite
          module Database.Algebra.Rewrite.DagRewrite
          -- * Rewrite rules
        , module Database.Algebra.Rewrite.Rule
+         -- * DAG matching
+       , module Database.Algebra.Rewrite.Match
+         -- * DAG traversal
+       , module Database.Algebra.Rewrite.Traversal
          -- * Pattern syntax
        , module Database.Algebra.Rewrite.PatternConstruction) where
 
 import Database.Algebra.Rewrite.DagRewrite
+import Database.Algebra.Rewrite.Match
 import Database.Algebra.Rewrite.Rule
+import Database.Algebra.Rewrite.Traversal
 import Database.Algebra.Rewrite.PatternConstruction(pattern, v)
