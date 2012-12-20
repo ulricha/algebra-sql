9a20163b4a8a2855afc319c07846a4989d8359b8
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-12-20 16:09:30 +0100

    More Data.IntMap fixes.

--- src/Database/Algebra/Aux.hs
+++ src/Database/Algebra/Aux.hs
@@ -15,5 +15,10 @@ lookupUnsafe m s u =
 replace :: Eq a => a -> a -> a -> a
 replace orig new x = if x == orig then new else x
 
+reverseToIntMap :: M.Map a Int -> IM.IntMap a
+reverseToIntMap m = IM.fromList $ map (\(a, b) -> (b, a)) $ M.toList m
+                
 reverseMap :: Ord b => M.Map a b -> M.Map b a
 reverseMap m = M.fromList $ map (\(a, b) -> (b, a)) $ M.toList m
+
+
