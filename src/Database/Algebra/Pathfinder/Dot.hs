bb74e0b43c2222474010531dad7146a52db16837
kaan <kaaninho@gmail.com>
2013-08-01 05:46:03 +0200

    Dot-Generator soweit fertig gestellt, Absprache bzgl. der einzelnen Operationen-Namen
    Convert: bekommt einen Plan, speichert eine PDF mit dessen
    Dot-Graphen, und öffnet diese anschließend

--- src/Database/Algebra/Pathfinder/Dot.hs
+++ src/Database/Algebra/Pathfinder/Dot.hs
@@ -10,7 +10,7 @@ import           Text.PrettyPrint 					 		--as Pretty
 import qualified Database.Algebra.Dag                		as Dag
 import           Database.Algebra.Dag.Common		 		--as DagC
 import           Database.Algebra.Pathfinder.Data.Algebra   as Alg
---import           Database.Algebra.X100.Render.Common
+
 
 nodeToDoc :: AlgNode -> Doc
 nodeToDoc n = (text "id:") <+> (int n)
@@ -18,82 +18,110 @@ nodeToDoc n = (text "id:") <+> (int n)
 tagsToDoc :: [Tag] -> Doc
 tagsToDoc ts = vcat $ map text ts
 
-labelToDoc :: AlgNode -> String -> [Tag] -> Doc
-labelToDoc n s ts = (nodeToDoc n) $$ (text s) <> (tagsToDoc $ nub ts)
+labelToDoc :: AlgNode -> String -> Doc -> [Tag] -> Doc
+labelToDoc n s as ts = (nodeToDoc n) $$ ((text s) <> (parens as)) $$ (tagsToDoc $ nub ts)
 
 
 lookupTags :: AlgNode -> NodeMap [Tag] -> [Tag]
 lookupTags n m = Map.findWithDefault [] n m
 
-
-
-renderBinOp :: BinOp -> Doc
-renderBinOp (Cross ())        = text "Cross"
-renderBinOp (EqJoin _)        = text "EqJoin"
-renderBinOp (Difference ())   = text "Diff"
-renderBinOp (DisjUnion ())    = text "Dis"
-renderBinOp (ThetaJoin _)     = text "theta"
-
-renderUnOp :: UnOp -> Doc
-renderUnOp (RowNum _)     = text "RowNum"
-renderUnOp (RowRank _)    = text "RowRank"
-renderUnOp (Proj _)       = text "Proj"
-renderUnOp (Sel _)        = text "Sel"
-renderUnOp (PosSel _)     = text "possel"
-renderUnOp (Distinct ())  = text "Distinct"
-renderUnOp (Attach _)     = text "Attach"
-renderUnOp (FunBinOp _)   = text "Funbin"
-renderUnOp (Cast _)       = text "Cast"
-renderUnOp (FunBoolNot _) = text "funboolnot"
-renderUnOp (Aggr _)       = text "aggr"
-renderUnOp (Dummy _)      = text "dummy"
-
-renderNullOp :: NullOp -> Doc
-renderNullOp (LitTable _ _) = text "LitTable"
-renderNullOp (EmptyTable _) = text "EmptyTable"
-renderNullOp (TableRef _)   = text "TableRef"
-
-data Op = NullaryOp | UnaryOp | BinOp 
---renderOp :: Op -> Doc
-
-
 bracketList :: (a -> Doc) -> [a] -> Doc
 bracketList f = brackets . hsep . punctuate comma . map f
 
+renderProj :: ProjPair -> Doc
+renderProj (new,old) = text $ concat [old,"->",new]
+
+-- | Rendering of the operations for opDotLabel
+renderPosSel :: SemInfPosSel -> Doc
+renderPosSel (i, sortInf, Just attr) = text $ show i ++ show sortInf ++ attr
+renderPosSel (i, sortInf, Nothing) = text $ show i ++ show sortInf
+
+renderFunBinOp :: SemBinOp -> Doc
+renderFunBinOp (fun, resAttr, leftAttr, rightAttr) = 
+    text $ resAttr ++ "  " ++ show fun 
+                   ++ ":" ++ leftAttr ++ "," ++ rightAttr
+
+renderAggr :: (AggrType, ResAttrName) -> Doc
+renderAggr (aggr, res) = text $ show aggr ++ ":" ++ res
+
+renderSortInf :: (SortAttrName, SortDir) -> Doc
+renderSortInf (attr, dir) = text $ attr ++ "/" ++ show dir
+
+renderTheta :: (LeftAttrName, RightAttrName, JoinRel) -> Doc
+renderTheta (left, right, joinR) = 
+    text $ show joinR ++ ":" ++ left ++ "," ++ right
+
+opDotLabel :: NodeMap [Tag] -> AlgNode -> PFLabel -> Doc
+-- | Nullary operations
+opDotLabel tags i (LitTableL _ _)             = labelToDoc i 
+    "LitTable" empty (lookupTags i tags)
+opDotLabel tags i (EmptyTableL _)             = labelToDoc i 
+    "EmptyTable" empty (lookupTags i tags)
+opDotLabel tags i (TableRefL _)               = labelToDoc i
+    "TableRef" empty (lookupTags i tags)
+-- |  Binary operations
+opDotLabel tags i (CrossL _)                  = labelToDoc i
+    "Cross" empty (lookupTags i tags)
+opDotLabel tags i (EqJoinL (left,right))      = labelToDoc i
+    "EqJoin" (text $ left ++ "," ++ right) (lookupTags i tags)
+opDotLabel tags i (DifferenceL _)             = labelToDoc i
+    "Difference" empty (lookupTags i tags)
+opDotLabel tags i (DisjUnionL _)              = labelToDoc i
+    "DisjUnion" empty (lookupTags i tags)
+opDotLabel tags i (ThetaJoinL info)           = labelToDoc i
+    "ThetaJoin" (bracketList renderTheta info) (lookupTags i tags)
+-- | Unary operations
+opDotLabel tags i (RowNumL (res,sortI,attr))  = labelToDoc i 
+    "RowNum" ((text $ res ++ "<")
+              $$ bracketList renderSortInf sortI 
+              $$ (text $ show attr ++ ">"))
+    (lookupTags i tags)
+opDotLabel tags i (RowRankL (res,sortInf))    = labelToDoc i
+    "RowRank" ((text $ res ++ "<") 
+               $$ bracketList renderSortInf sortInf
+               $$ text "<")                
+    (lookupTags i tags)
+opDotLabel tags i (RankL (res,sortInf))       = labelToDoc i
+    "Rank" ((text $ res ++ "<") 
+            $$ bracketList renderSortInf sortInf
+            $$ text ">")                
+    (lookupTags i tags)
+opDotLabel tags i (ProjL info)                = labelToDoc i 
+    "Project" (bracketList renderProj info) (lookupTags i tags)
+opDotLabel tags i (SelL info)                 = labelToDoc i
+    "Select" (text info) (lookupTags i tags)
+opDotLabel tags i (PosSelL info)              = labelToDoc i
+    "PosSel" (renderPosSel info) (lookupTags i tags)
+opDotLabel tags i (DistinctL _)               = labelToDoc i
+    "Distinct" empty (lookupTags i tags)
+opDotLabel tags i (AttachL (res, (aty,aval))) = labelToDoc i
+    "Attach" (text $ res ++ "," ++ show aty ++ ":" ++ show aval)
+    (lookupTags i tags)
+opDotLabel tags i (FunBinOpL info)            = labelToDoc i
+    "FunBinOp" (renderFunBinOp info) (lookupTags i tags) 
+opDotLabel tags i (CastL (res,attr,aty))      = labelToDoc i
+    "Cast" (text $ res ++ "," ++ show aty ++ ":" ++ attr) (lookupTags i tags)
+opDotLabel tags i (FunBoolNotL (res,attr))    = labelToDoc i
+    "FunBoolNot" (text $ res ++ "," ++ attr) (lookupTags i tags)
+opDotLabel tags i (AggrL (aggrList, attr))    = labelToDoc i
+    "Aggr" ((bracketList renderAggr aggrList) $$ (text $ show attr))
+    (lookupTags i tags)
+opDotLabel _ _ (DummyL _)                     = text "dummy"
 
 
 
-bracketListLines :: (a -> Doc) -> [a] -> Doc
-bracketListLines f = brackets . vcat . punctuate comma . map f
-
-
-escapeLabel :: String -> String
-escapeLabel s = concatMap escapeChar s
-
-escapeChar :: Char -> [Char]
-escapeChar '\n' = ['\\', 'n']
-escapeChar '\\' = ['\\', '\\']
-escapeChar '\"' = ['\\', '"']
-escapeChar c = [c]
-
-
-
-constructDotNode :: [AlgNode] -> NodeMap [Tag] -> (AlgNode, PFLabel) -> DotNode
-constructDotNode rootNodes ts (n, op) =
+constructDotNode :: NodeMap [Tag] -> (AlgNode, PFLabel) -> DotNode
+constructDotNode tags (n, op) =
     DotNode n l c Nothing
-      where l = "F" -- escapeLabel $ render $ opDotLabel ts n op
-            c = Tomato
+      where l = render $ opDotLabel tags n op
+            c = opDotColor op
 
 -- | Create an abstract Dot edge
 constructDotEdge :: (AlgNode, AlgNode) -> DotEdge
 constructDotEdge = uncurry DotEdge
 
-
-
 renderDotEdge :: DotEdge -> Doc
 renderDotEdge (DotEdge u v) = int u <+> text "->" <+> int v <> semi
-
-
 
 renderColor :: DotColor -> Doc
 renderColor Tomato = text "tomato"
@@ -110,7 +138,31 @@ renderColor Beige = text "beige"
 renderColor DodgerBlue = text "dodgerblue"
 renderColor LightSkyBlue = text "lightskyblue"
 
-
+opDotColor :: PFLabel -> DotColor
+-- | Nullaryops
+opDotColor (EmptyTableL _)   = Gray
+opDotColor (LitTableL _ _)   = Gray
+opDotColor (TableRefL _)     = Gray
+-- | Unops
+opDotColor (AggrL _)         = Gold
+opDotColor (AttachL _)       = Gold
+opDotColor (CastL _)         = Gold
+opDotColor (DistinctL _)     = Gold
+opDotColor (DummyL _)        = Gold
+opDotColor (FunBoolNotL _)    = Gold
+opDotColor (FunBinOpL _)     = Gold
+opDotColor (ProjL _)         = Gold
+opDotColor (RankL _)         = Gold
+opDotColor (RowNumL _)       = Gold
+opDotColor (RowRankL _)      = Gold
+opDotColor (PosSelL _)       = Gold
+opDotColor (SelL _)          = Gold
+-- | Binops
+opDotColor (CrossL     _)    = Crimson
+opDotColor (DifferenceL _)   = Crimson
+opDotColor (DisjUnionL _)    = Crimson
+opDotColor (EqJoinL    _)    = Crimson
+opDotColor (ThetaJoinL _)    = Crimson
 
 renderDotNode :: DotNode -> Doc
 renderDotNode (DotNode n l c s) =
@@ -125,17 +177,12 @@ renderDotNode (DotNode n l c s) =
                   Just Solid -> comma <+> text "solid"
                   Nothing -> empty
 
-
--- | für renderDot
-
--- Generate the preamble of a Dot file
 preamble :: Doc
 preamble = graphAttributes $$ nodeAttributes
     where nodeAttributes = text "node" <+> (brackets $ text "style=filled" <> comma <+> text "shape=box") <> semi
           graphAttributes = text "ordering=out;"
 
-
--- Dot colors
+-- | Dot colors
 data DotColor = Tomato
               | Salmon
               | Gray
@@ -166,72 +213,67 @@ data DotNode = DotNode DotNodeID DotLabel DotColor (Maybe DotStyle)
 -- Type of Dot edges
 data DotEdge = DotEdge DotNodeID DotNodeID
 
-
-
-
--- | Render a Dot document from the preamble, nodes and edges
+-- |  Render a Dot document from the preamble, nodes and edges
 renderDot :: [DotNode] -> [DotEdge] -> Doc
 renderDot ns es = text "digraph" <> (braces $ preamble $$ nodeSection $$ edgeSection)
     where nodeSection = vcat $ map renderDotNode ns
           edgeSection = vcat $ map renderDotEdge es
 
-
-
--- | für Label Type
-data PFLabel = EmptyTableL
-			 | LitTableL
-			 | TableRefL	-- nullops
-			 | AggrL
-			 | AttachL
-			 | CastL
-			 | DistinctL
-			 | DummyL
-			 | FunBinOpL
-			 | FunBoolNotL
-			 | ProjL
-			 | RankL
-			 | RowNumL
-			 | RowRankL
-			 | PosSelL 			-- unops
-			 | CrossL
-			 | DifferenceL
-			 | DisjUnionL
-			 | EqJoinL
-			 | ThetaJoinL 	-- binops
+-- |Labels (to collect all operations (nullary, unary,binary))
+data PFLabel = EmptyTableL SchemaInfos
+      			 | LitTableL SemInfLitTable SchemaInfos
+			       | TableRefL SemInfTableRef    -- nullops
+			       | AggrL SemInfAggr
+			       | AttachL SemInfAttach
+			       | CastL SemInfCast
+			       | DistinctL ()
+			       | DummyL String
+			       | FunBinOpL SemBinOp
+			       | FunBoolNotL SemUnOp
+			       | ProjL SemInfProj
+			       | RankL SemInfRank
+			       | RowNumL SemInfRowNum
+			       | RowRankL SemInfRank
+             | SelL SemInfSel
+			       | PosSelL SemInfPosSel        -- unops
+			       | CrossL ()
+			       | DifferenceL ()
+			       | DisjUnionL ()
+			       | EqJoinL SemInfEqJoin
+			       | ThetaJoinL SemInfThetaJoin  -- binops
 
 labelOfOp :: PFAlgebra -> PFLabel
-labelOfOp (TerOp _ _ _ _)   = error "keine tertiäre Operation"
+labelOfOp (TerOp _ _ _ _)   = error "no tertiary operations"
 labelOfOp (Database.Algebra.Dag.Common.BinOp op _ _)	= labelOfBinOp op
 labelOfOp (Database.Algebra.Dag.Common.UnOp op _)		= labelOfUnOp op
 labelOfOp (Database.Algebra.Dag.Common.NullaryOp op)	= labelOfNullaryOp op
 
 labelOfBinOp :: BinOp -> PFLabel
-labelOfBinOp (Cross _)     		= CrossL
-labelOfBinOp (Difference ())  = DifferenceL
-labelOfBinOp (DisjUnion ()) 	= DisjUnionL
-labelOfBinOp (EqJoin _)	    	= EqJoinL
-labelOfBinOp (ThetaJoin _)   	= ThetaJoinL
+labelOfBinOp (Cross info)     	= CrossL info
+labelOfBinOp (Difference info)  = DifferenceL info
+labelOfBinOp (DisjUnion info) 	= DisjUnionL info
+labelOfBinOp (EqJoin info)	    = EqJoinL info
+labelOfBinOp (ThetaJoin info)   = ThetaJoinL info
 
 labelOfUnOp :: UnOp -> PFLabel
-labelOfUnOp (Aggr _) 		   	= AggrL
-labelOfUnOp (Attach _) 	   	= AttachL
-labelOfUnOp (Cast _) 		   	= CastL
-labelOfUnOp (Distinct _) 		= DistinctL
-labelOfUnOp (Dummy _) 	   	= DummyL
-labelOfUnOp (FunBinOp _) 		= FunBinOpL
-labelOfUnOp (FunBoolNot _) 	= FunBoolNotL
-labelOfUnOp (PosSel _)      = PosSelL
-labelOfUnOp (Proj _) 			  = ProjL
-labelOfUnOp (Rank _) 		   	= RankL
-labelOfUnOp (RowNum _) 	  	= RowNumL
-labelOfUnOp (RowRank _) 		= RowRankL
+labelOfUnOp (Aggr info) 		   	= AggrL info
+labelOfUnOp (Attach info)	   	  = AttachL info
+labelOfUnOp (Cast info) 		   	= CastL info
+labelOfUnOp (Distinct info) 		= DistinctL  info
+labelOfUnOp (Dummy info) 	   	  = DummyL info
+labelOfUnOp (FunBinOp info) 		= FunBinOpL info
+labelOfUnOp (FunBoolNot info) 	= FunBoolNotL info
+labelOfUnOp (PosSel info)       = PosSelL info
+labelOfUnOp (Proj info) 			  = ProjL info
+labelOfUnOp (Rank info) 		   	= RankL info
+labelOfUnOp (RowNum info) 	  	= RowNumL info
+labelOfUnOp (RowRank info) 	  	= RowRankL info
+labelOfUnOp (Sel info)          = SelL info
 
 labelOfNullaryOp :: NullOp -> PFLabel
-labelOfNullaryOp (EmptyTable _)	= EmptyTableL
-labelOfNullaryOp (LitTable _ _)	= LitTableL
-labelOfNullaryOp (TableRef _) 	= TableRefL
-
-
+labelOfNullaryOp (EmptyTable  info)	      = EmptyTableL info
+labelOfNullaryOp (LitTable  info1 info2)	= LitTableL info1 info2
+labelOfNullaryOp (TableRef  info)      	  = TableRefL info
 
 -- | extract the operator descriptions and list of edges from a DAG
 
@@ -244,16 +286,10 @@ extractGraphStructure toLabel d = (labels, childs)
           labels = map (\(n, op) -> (n, toLabel op)) operators
           childs = concat $ map (\(n, op) -> zip (repeat n) (Dag.opChildren op)) operators
 
-
-
 -- | Render an PFAlgebra plan into a dot file (GraphViz).
 renderPFDot :: NodeMap [Tag] -> [AlgNode] -> NodeMap PFAlgebra -> String
 renderPFDot ts roots m = render $ renderDot dotNodes dotEdges
     where (opLabels, edges) = extractGraphStructure labelOfOp d
           d = Dag.mkDag m roots
-          dotNodes = map (constructDotNode roots ts) opLabels
+          dotNodes = map (constructDotNode ts) opLabels
           dotEdges = map constructDotEdge edges
-
-
-
-
